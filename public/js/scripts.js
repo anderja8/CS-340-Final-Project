ed70d76856742c04d1b15c063712980bb51ff7b9
John <rudolpjo@oregonstate.edu>
2020-02-24 19:41:34 -0700

    script comments for step 5

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,14 @@
+//Complete for Step 5
 function addState() {
 	console.log("Function addState() was called.")
 }
 
+//Complete for Step 5
 function addArea() {
 	console.log("Function addArea() was called.")
 }
 
+//Complete for Step 5
 function addRoute() {
 	console.log("Function addRoute() was called.")
 }
@@ -16,6 +19,11 @@ function updateUser() {
 
 function deleteUser() {
 	console.log("Function deleteUser() was called.")
+}
+
+//Complete for Step 5
+function addRating() {
+	console.log("Function addRating() was called.")
 }
 
 function updateRating() {
